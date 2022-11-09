5840507dc449bde9387eeb9698d8fa7b371d7da1
Michaël COLL <michael.coll@hiit-consulting.fr>
2022-11-09 17:40:33 +0100

    fix(web) : Fix static file resolution
    - Add cache-control headers on static files

--- internal/web/static.go
+++ internal/web/static.go
@@ -3,11 +3,7 @@ package web
 import "embed"
 
 //go:embed dist/assets
+//go:embed dist/favicon
 //go:embed dist/img
-//go:embed dist/apple-touch-icon.png
-//go:embed dist/favicon.ico
-//go:embed dist/icon-192.png
-//go:embed dist/icon-512.png
 //go:embed dist/index.html
-//go:embed dist/manifest.webmanifest
 var Static embed.FS
